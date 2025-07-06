bace3ef051b75089906ce4bc0f21d5593664a249
Lucas Aguiar <lucasaguiar.98@outlook.com>
2025-07-05 23:00:29 -0300

    transformando em menu lateral e adicionando efeito ao elementos que forem surgindo na tela

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 const btn_menu = document.querySelector("#btn-menu");
+const btn_close = document.querySelector("#btn-close");
+const sidebar = document.querySelector('.sidebar');
+const main = document.querySelector(".main");
+const header = document.querySelector(".header");
+
 
 btn_menu.addEventListener('click', ()=>{
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+    sidebar.classList.toggle('active');
+    main.style.opacity = '0.3';
+    header.style.opacity = '0.3';
+    document.body.style.overflow = 'hidden';
 })
+
+btn_close.addEventListener('click', ()=> {
+    sidebar.classList.remove("active");
+    main.style.opacity = '1';
+    header.style.opacity = '1';
+    document.body.style.overflow = 'auto';
+
+
+})
+
+// script das animações
+AOS.init();
